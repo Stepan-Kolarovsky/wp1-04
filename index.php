86e0ff3850b395c8107b1c414bb18ef6a14d7776
Stepan-Kolarovsky <stepan.kolarovsky@student.ossp.cz>
2020-03-03 09:24:43 +0100

    Update index.php
    přidání obrázků

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
   $age=18;
 if ($age >=18) {
-    $odpoved="Děkujeme za kšeft. Přiďte zas.";
+    $answer="Děkujeme za kšeft. Přiďte zas.";
+    $imgURL="https://www.ruskolobok.cz/i/?i=4601728011086.jpg";
 } else {
-    $odpoved="Máš smůlu chlapče, alkohol a tabák prodáváme osobám starším 18 let";
+    $answer="Máš smůlu chlapče, alkohol a tabák prodáváme osobám starším 18 let";
+    $imgURL="https://previews.123rf.com/images/axis213/axis2131802/axis213180200004/94912552-no-alcohol-drink-sign-vector-logo-element-no-drinking-sign-no-alcohol-sign-isolated-on-white-backgro.jpg" ;
+
 }
 
 
@@ -19,6 +22,8 @@ if ($age >=18) {
     <title>Document</title>
 </head>
 <body>
-     <?=$odpoved ?>   
+     <?=$answer ?> 
+     <img src="<?=$imgURL?>">
+
 </body>
 </html>
